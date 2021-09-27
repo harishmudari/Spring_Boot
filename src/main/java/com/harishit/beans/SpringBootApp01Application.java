3118bdfd6b5ffc5d287dc90b49f0fe028771fd90
harish479 <harishmudari@gmail.com>
2021-09-27 23:55:34 +0530

    modified as a part of cls3 :

---
+++ src/main/java/com/harishit/beans/SpringBootApp01Application.java
@@ -0,0 +1,14 @@
+package com.harishit.beans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp01Application.class, args);
+		System.out.println("Hello World..!!");
+	}
+
+}
